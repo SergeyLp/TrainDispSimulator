5792112e36df7277101cb4d26f7a8afad90cae9a
Sergej Lipatov <serreg123@gmail.com>
2018-02-05 01:05:27 +0300

    Clean code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,39 +16,43 @@ namespace Ralway{
                 block,
                 junction,
             };
-            public RType type_;
-            public NamedNode prev_ { get; set; } //указатели узла
-            public NamedNode next_ { get; set; }
-            public string name_ { get; set; }  //вставляемое значение
+            public RType type_ { get; set; }
+            public NamedNode prev { get; set; }
+            public NamedNode next { get; set; }
+            public string name { get; set; }
+
+            public NamedNode(string new_name, RType type = RType.block, Block prev = null, Block next = null){
+                name = new_name;
+                type_ = type;
+                this.prev = prev;
+                this.next = next;
+            }
         }
 
         public class Block : NamedNode {
             public double len_;
 
-            public Block(string name, double len, RType type = RType.block, Block prev = null, Block next = null) {
-                name_ = name;
+            public Block(string name, double len, RType type = RType.block, Block prev = null, Block next = null)
+                :base(name, type, prev, next) {
                 len_ = len;
-                type_ = type;
-                prev_ = prev;
-                next_ = next;
             }
 
-            public new string ToString() {
-                return String.Format("{0,12} {1,4}", name_, len_.ToString("0.0"));
-            }
+            public new string ToString() => String.Format("{0,12} {1,4}", name, len_.ToString("0.0"));
+            
         }
 
         public LinkedList<Block> list = new LinkedList<Block>();
 
         public  void Dump() {
             double dist = 0;
-            foreach(var li in list) {
+            Trace.WriteLine("Branch dump");
+            foreach (var li in list) {
                 string s = li.ToString();
 
                 dist += li.len_;
                 s += "\t";
                 s += dist.ToString();
-                Console.WriteLine(String.Format("{0}  {1,5}", li.ToString(), dist.ToString("0.0") ));
+                Trace.WriteLine(String.Format("{0}  {1,5}", li.ToString(), dist.ToString("0.0") ));
             }
         }
 
@@ -59,16 +63,13 @@ namespace Ralway{
     }
 
 
-    class Program{
-
-
+    class Game{
         static void Main(string[] args){
-            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
             Branch branch = new Branch();
-            double last_dist = 0;
             SortedList<double, string> stations_map = new SortedList<double, string>();
             using (StreamReader reader = File.OpenText(@"D:\Win7\lis\Documents\Dev\RW\Выборг.layout")){
                 string text;
+                double last_dist = 0;
                 while ((text = reader.ReadLine()) != null) {
                     if (text.Length < 1 || text[0] == '#') {
                         continue;
@@ -100,28 +101,29 @@ namespace Ralway{
             }
             IList<double> speed_points = speed_map.Keys;
 
-            double a_loc = 0.25;    // m/s^2
-            const double v_max_loc = 120 / 3.6; // m/s
+            double a_loco = 0.2;    // m/s^2
+            const double speed_max_loco_restrict = 120 / 3.6; // m/s
 
             SortedList<double, double> speed_prepare_list = new SortedList<double, double>();
             double v_prev =0;
+            Trace.WriteLine("Table prepare speed restrict");
             foreach (var dist in speed_points) {
                 double vx = speed_map[dist]/3.6;
                 double dv = vx - v_prev;
                 if (dv < 0) {
-                    double t = -dv / a_loc;
-                    double dist_prep = v_prev * t + a_loc * t * t / 2;
-                    Console.Write("{0} {1} {2} {3}\n", v_prev * 3.6, vx * 3.6, dist - dist_prep/1000, dist_prep);
+                    double t = -dv / a_loco;
+                    double dist_prep = v_prev * t + a_loco * t * t / 2;
+                    Trace.Write(String.Format("{0} {1} {2:#,0.000} {3}\n", v_prev * 3.6, vx * 3.6, dist - dist_prep/1000, (int)dist_prep));
                     speed_prepare_list.Add(dist*1000 - dist_prep, vx);
                 }
                 v_prev = vx;
             }
             IList<double> speed_prep_points = speed_prepare_list.Keys;
 
-            //branch.Dump();
+            branch.Dump();
 
             DateTime date0 = new DateTime(2009, 5, 1, 16, 30, 0);
-            //Console.Write("{0}\n", date0);
+            Console.Write("{0}\n", date0);
 
             IList<double> time_points = stations_map.Keys;
             int station_number = 0;
@@ -131,46 +133,52 @@ namespace Ralway{
             double dp = 0, pos = 0; // m
             double sec_from_0 = 0;  // s
             double dt = 0.05;    // s
-            double speed_restict = 0;   // m/s
+            double current_speed_restict = 0;   // m/s
             double next_speed_restict = 0;   // m/s
             int speed_prep_index = 0;
             double speed_prep_dist = 0;
-            double a = a_loc;
+            double a = a_loco;
             for (; ; ) {
                 
                 if ((speed_index < speed_points.Count) && (pos >= speed_points[speed_index] * 1000)) {
-                    speed_restict = speed_map[speed_points[speed_index++]] / 3.6;
-                    a = +a_loc;
-                    if (speed_index < speed_points.Count)
+                    current_speed_restict = speed_map[speed_points[speed_index++]] / 3.6;
+                    current_speed_restict = (current_speed_restict > speed_max_loco_restrict)
+                                    ? speed_max_loco_restrict
+                                    : current_speed_restict;
+                    a = +a_loco;
+                    if (speed_index < speed_points.Count) {
                         next_speed_restict = speed_map[speed_points[speed_index]] / 3.6;
+                        next_speed_restict = (next_speed_restict > speed_max_loco_restrict)
+                                        ? speed_max_loco_restrict
+                                        : next_speed_restict;
+
+                    }
                 }
 
                 if ((speed_prep_index < speed_prep_points.Count) && (pos >= speed_prep_points[speed_prep_index])) {
                     speed_prep_dist = speed_prep_points[speed_prep_index++];
-                    a = -a_loc;
+                    a = -a_loco;
                 }
 
-
                 v = v0 + a * dt;
-
-                if ((a < 0) && (v < next_speed_restict))
-                    a = 0;
-
-                v = (v <= speed_restict) ? v : speed_restict;
-                v = (v <= v_max_loc) ? v : v_max_loc;
-
                 dp = (v0 + v)/2 * dt ;
                 pos += dp;
                 v0 = v;
 
+                if ((a < 0) && (v <= next_speed_restict) && (v <= current_speed_restict))
+                    a = 0;
+
+                if (v >= current_speed_restict)
+                    a = 0;
+
                 DateTime date = date0.AddSeconds(sec_from_0);
-                Console.Write("{0:T}\t{1:##0.00}\t{2,3}", date, pos/1000, (int)(v * 3.6));
+                Console.Write("{0:t}\t{1:##0.000}\t{2,3}", date, pos/1000, (int)(v * 3.6));
                 if (pos >= time_points[station_number] * 1000) {
                     Console.Write("  {0}\n", stations_map[time_points[station_number]]);
                     if (++station_number == time_points.Count) break;
                 } else
                     Console.Write("\r");
-                Thread.Sleep(5);
+                //Thread.Sleep(5);
                 sec_from_0 += dt;
             }
             Console.Write("\n\t\t<<PRESS ENTER>>");
